ee4b6cc3e0f7e9744e049648284ead8edb43d43a
Rob Kenis <rob.kenis@hotmail.com>
2024-10-14 12:16:43 +0200

    Display namespace in frontend

--- cmd/server.go
+++ cmd/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"html/template"
 	"net/http"
 	"os"
 	"time"
@@ -14,15 +15,48 @@ type health struct {
 	Status string `json:"status"`
 }
 
+const templ = `<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Workshop GitOps on Kubernetes</title>
+</head>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+</style>
+
+<body>
+	<div class="container">
+    	<h1>Workshop GitOps on Kubernetes</h1>\
+		<p>I am running in the <strong>{{ .Namespace }}</strong> namespace</p>
+	</div>
+</body>
+
+</html>
+`
+
 func main() {
 	log.Logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.TimeOnly}).With().Timestamp().Logger()
 
 	r := http.NewServeMux()
 
-	r.Handle("GET /health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	r.Handle("GET /healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		_ = json.NewEncoder(w).Encode(health{Status: "UP"})
+	}))
+
+	r.Handle("GET /", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+		t, _ := template.New("index").Parse(templ)
+		_ = t.Execute(w, map[string]string{"Namespace": os.Getenv("NAMESPACE")})
 	}))
 
 	srv := &http.Server{
