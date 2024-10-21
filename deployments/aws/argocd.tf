2132e1be8e880f9201b4f0aa70fa4a12007b1fa4
Rob Kenis <rob.kenis@hotmail.com>
2024-10-21 15:14:40 +0200

    Install ArgoCD

---
+++ deployments/aws/argocd.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "argocd" {
+  namespace        = "argocd"
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "7.6.12"
+  wait             = false
+  create_namespace = true
+
+  values = [templatefile("${path.module}/argocd.values.yaml", {})]
+}
