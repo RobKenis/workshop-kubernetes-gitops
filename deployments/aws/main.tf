94edc2942a9780f167112350878aa52e3d458834
Rob Kenis <rob.kenis@hotmail.com>
2024-11-01 12:39:31 +0100

    Deploy LB controller

--- deployments/aws/main.tf
+++ deployments/aws/main.tf
@@ -85,3 +85,30 @@ module "karpenter" {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   }
 }
+
+resource "aws_iam_role" "lb_controller" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = ["sts:AssumeRole", "sts:TagSession"]
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lb_controller" {
+  role   = aws_iam_role.lb_controller.name
+  policy = file("${path.module}/policies/lb_controller.json")
+}
+
+resource "aws_eks_pod_identity_association" "lb_controller" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "kube-system"
+  service_account = "aws-load-balancer-controller"
+  role_arn        = aws_iam_role.lb_controller.arn
+}
