e7d6cd0da5f52bb044a19fe98a12594da28594e1
Rob Kenis <rob.kenis@hotmail.com>
2024-11-01 11:54:55 +0100

    Install Karpenter

--- deployments/aws/main.tf
+++ deployments/aws/main.tf
@@ -67,3 +67,17 @@ module "eks" {
     Terraform = "true"
   }
 }
+
+module "karpenter" {
+  source = "terraform-aws-modules/eks/aws//modules/karpenter"
+
+  cluster_name = module.eks.cluster_name
+
+  create_pod_identity_association = true
+  namespace                       = "karpenter"
+
+  # Attach additional IAM policies to the Karpenter node IAM role
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+}
