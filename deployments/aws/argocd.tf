b496c4ab0a428700894403f043472b2ae46d78fc
Rob Kenis <rob.kenis@hotmail.com>
2024-11-01 11:46:22 +0100

    Automate bootstrap of Argo repositories

--- deployments/aws/argocd.tf
+++ deployments/aws/argocd.tf
@@ -7,7 +7,20 @@ resource "helm_release" "argocd" {
   wait             = false
   create_namespace = true
 
-  values = [templatefile("${path.module}/argocd.values.yaml", {})]
+
+  values = [
+    templatefile("${path.module}/templates/argocd.values.yaml", {
+      sshPrivateKey = indent(8, data.aws_secretsmanager_secret_version.github_ssh_key.secret_string)
+    })
+  ]
 
   depends_on = [ module.eks ]
+}
+
+data "aws_secretsmanager_secret" "github_ssh_key" {
+  name = "github/robkenis/workshop-kubernetes-gitops/deploy-key"
+}
+
+data "aws_secretsmanager_secret_version" "github_ssh_key" {
+  secret_id = data.aws_secretsmanager_secret.github_ssh_key.id
 }
